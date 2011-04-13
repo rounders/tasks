c4b2d1b70b7647b32e91ba8a66a60bae34925726
Francois Harbec <fharbec@gmail.com>
2011-04-13 12:02:59 -0700

    begin setting up tasks_controller functional tests

--- test/functional/tasks_controller_test.rb
+++ test/functional/tasks_controller_test.rb
@@ -3,12 +3,46 @@ require 'test_helper'
 class TasksControllerTest < ActionController::TestCase
   def setup
     @user = User.create(:email => 'user@user.com', :password => 'abc123', :password_confirmation => 'abc123')
-    # create 5 -  10 sample projects
-    @number_of_projects = 5 + rand(5)
-    @number_of_projects.times do |i|
-      @user.projects.create(:name => "Project #{i}")
+    @project = @user.projects.create(:name => "My Project")
+
+    # create 5 -  10 sample tasks
+    @number_of_tasks = 5 + rand(5)
+    @number_of_tasks.times do |i|
+      @project.tasks.create(:description => "Task #{i}")
     end
   end
   
+  test "should redirect to sign in page if not logged in" do
+    get :new, :project_id => @project
+    assert_redirected_to new_user_session_path
+    
+    post :create, :project_id => @project, :task => { :description => 'my test project' }
+    assert_redirected_to new_user_session_path
+  end
   
+  test "should display new task form" do 
+    sign_in @user
+    get :new, :project_id => @project
+    assert_response :success
+    assert_select 'form#new_task'
+  end
+  
+  test "should create new task" do
+    sign_in @user
+    assert_difference('Task.count') do
+      post :create, :project_id => @project, :task => { :description => 'my test project' }
+    end
+    
+    assert assigns(:project)
+    assert assigns(:task)
+    assert_equal assigns(:task).description, 'my test project'
+    assert_redirected_to project_path(assigns(:project))
+  end
+  
+  test "should update an existing task" do
+    sign_in @user
+    task = @project.tasks.sample
+  end
+  
+
 end
